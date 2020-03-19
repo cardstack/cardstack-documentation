e97adbe43447a094b13759f224578c9380c61a12
Jen Weber <j@jenweber.me>
2020-03-19 10:24:26 -0400

    Add link checking tests - copied from ember guides-source

--- node-tests/link-checker.js
+++ node-tests/link-checker.js
@@ -1,6 +1,52 @@
-/* eslint-env node, mocha */
-const guidemakerLinkChecker = require('guidemaker-link-checker');
+const { expect } = require('chai');
+
+
+/**
+ * Autogenerate some mocha tests
+ */
+
+const walkSync = require('walk-sync');
+const { extname } = require('path');
+const { inspect } = require('util');
+const {
+  findMarkdownLinks,
+  getBadRelativeUrlsForFile,
+  getBadLineBreaks,
+  getBadImageUrls,
+  getNonRelativeGuidesLinks,
+} = require('./helpers');
+
+const paths = walkSync('guides')
+  .filter(filePath => extname(filePath) === '.md')
+  .map(filePath => `guides/${filePath}`);
+
+function printBadLinks(badLinks) {
+  return inspect(badLinks, { depth: null });
+}
 
 describe('check all links in markdown files', function () {
-  guidemakerLinkChecker();
+  paths.forEach((filepath) => {
+    it(`processing ${filepath}`, function () {
+      const links = findMarkdownLinks(filepath);
+      const badLinks = getBadRelativeUrlsForFile({
+        filepath,
+        links,
+      });
+      const nonRelativeGuidesLinks = getNonRelativeGuidesLinks(filepath, links);
+      expect(nonRelativeGuidesLinks, printBadLinks(nonRelativeGuidesLinks)).to.be.empty;
+
+      expect(badLinks, printBadLinks(badLinks)).to.be.empty;
+
+      const badLineBreaks = getBadLineBreaks(filepath);
+      expect(badLineBreaks, printBadLinks(badLineBreaks)).to.be.empty;
+
+      const badImageLinks = getBadImageUrls({
+        filepath,
+        links,
+      });
+
+      expect(badImageLinks, printBadLinks(badImageLinks)).to.be.empty;
+    });
+  });
 });
+
